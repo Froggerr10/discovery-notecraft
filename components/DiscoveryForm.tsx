a5f0a1f1ede31c470c7a8f98a14816132bb7b65a
David De Cunto <124645977+Froggerr10@users.noreply.github.com>
2025-06-30 20:28:54 -0300

    fix: resolve JSX syntax error in DiscoveryForm component

--- components/DiscoveryForm.tsx
+++ components/DiscoveryForm.tsx
@@ -245,7 +245,7 @@ export default function DiscoveryForm({
     <div className={`min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 ${className}`}>
       {/* Background Pattern */}
       <div className="fixed inset-0 opacity-30">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.05"%3E%3Ccircle cx="7" cy="7" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.05\"%3E%3Ccircle cx=\"7\" cy=\"7\" r=\"1\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat"></div>
       </div>
 
       <div className="relative z-10 container mx-auto px-4 py-8 max-w-6xl">
